94576f0da1d0c11f4050e122741a2b31efdc2e5d
Hugo EXTRAT <extrat.h@gmail.com>
2025-08-25 09:13:34 +0200

    fix(minor): fix ios new arch

--- ios/ScreenshotAware.h
+++ ios/ScreenshotAware.h
@@ -1,7 +1,7 @@
 #import <React/RCTEventEmitter.h>
 
 #ifdef RCT_NEW_ARCH_ENABLED
-#import "generated/ScreenshotAwareSpec/ScreenshotAwareSpec.h"
+#import "ScreenshotAwareSpec/ScreenshotAwareSpec.h"
 
 @interface ScreenshotAware : RCTEventEmitter <RCTBridgeModule>
 #else
